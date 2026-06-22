9d3470106ec85fdf61504126f10880f90b365453
User <user@localhost>
2026-06-22 23:45:34 +0300

    Remove opencode-android from repository (planned, coming later)

--- opencode-android/aapt2_wrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Wrapper for running x86-64 AAPT2 on ARM64 via QEMU
-QEMU=/usr/bin/qemu-x86_64-static
-ROOTFS=/полностью\ рабочее\ с\ openhands,\ openwebui/.local/ubuntu-amd64-root
-AAPT2=$HOME/Android/Sdk/build-tools/34.0.0/aapt2
-
-exec $QEMU -L "$ROOTFS" "$AAPT2" "$@"
--- opencode-android/app/src/main/java/com/hermes/gui/HermesApp.kt
+++
@@ -1,7 +0,0 @@
-package com.hermes.gui
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class HermesApp : Application()
